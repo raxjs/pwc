868df701b447549050e6ed2514c05866bc6b1513
fushen <fushen.jzw@alibaba-inc.com>
2022-03-22 17:22:35 +0800

    chore: remove useless log

--- packages/pwc/src/elements/reactiveElementFactory.ts
+++ packages/pwc/src/elements/reactiveElementFactory.ts
@@ -23,7 +23,6 @@ export default (Definition) => {
     // Custom element native lifecycle
     connectedCallback() {
       if (!this.#initialized) {
-        console.log('this.__init_task__ ====> ', this.__init_task__);
         if (this.__init_task__) {
           this.__init_task__();
         }
