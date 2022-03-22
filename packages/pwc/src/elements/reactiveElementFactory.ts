16c840659e6164ccf7ce2744f5ba424643c488c3
Rongyan Chen <chenrongyan.cry@alibaba-inc.com>
2022-03-22 01:12:38 -0700

    fix: replace number with Date timestamp (#19)
    * fix: replace number with Date timestamp
    
    * fix: private field
    
    * chore: up lockfile

--- packages/pwc/src/elements/reactiveElementFactory.ts
+++ packages/pwc/src/elements/reactiveElementFactory.ts
@@ -3,15 +3,12 @@ import { TEXT_COMMENT_DATA, PWC_PREFIX, PLACEHOLDER_COMMENT_DATA } from '../cons
 import { Reactive } from '../reactivity/reactive';
 import type { ReactiveNode } from './reactiveNode';
 import { AttributedNode, TextNode } from './reactiveNode';
-import { shallowEqual } from '../utils';
-import type { SchedulerJob } from './sheduler';
+import { shallowEqual, generateUid } from '../utils';
 import { queueJob } from './sheduler';
-
-let uid = 0;
 
 export default (Definition) => {
   return class extends Definition implements PWCElement {
-    uid: number = uid++;
+    #uid: number = generateUid();
     // Component initial state
     #initialized = false;
     // The root fragment
@@ -22,11 +19,6 @@ export default (Definition) => {
     #reactiveNodes: ReactiveNode[];
     // Reactive instance
     #reactive: Reactive = new Reactive(this);
-    // update job
-    updateJob: SchedulerJob = {
-      uid: this.uid,
-      run: this.#performUpdate.bind(this),
-    };
 
     // Custom element native lifecycle
     connectedCallback() {
@@ -102,7 +94,10 @@ export default (Definition) => {
     }
 
     requestUpdate(): void {
-      queueJob(this.updateJob);
+      queueJob({
+        uid: this.#uid,
+        run: this.#performUpdate.bind(this),
+      });
     }
 
     getReactiveValue(prop: string): unknown {
