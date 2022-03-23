2c460dc6bad3aa7c745068761de427b8d8156384
fushen <fushen.jzw@alibaba-inc.com>
2022-03-23 11:41:03 +0800

    fix: safe alert

--- examples/basic/vite.config.ts
+++ examples/basic/vite.config.ts
@@ -1,11 +1,33 @@
 import { defineConfig } from 'vite';
 import babel from 'vite-plugin-babel';
-import * as babelConfig from '../../babel.config.js';
 
 export default defineConfig({
   plugins: [
     babel({
-      babelConfig,
+      babelConfig: {
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              targets: {
+                chrome: 99,
+              },
+              modules: false,
+            },
+          ],
+        ],
+        plugins: [
+          [
+            '@babel/plugin-proposal-decorators',
+            {
+              version: '2021-12',
+            },
+          ],
+          '@babel/plugin-proposal-class-properties',
+          '@babel/plugin-proposal-class-static-block',
+          '@babel/plugin-proposal-private-methods'
+        ],
+      },
     }),
   ],
 });
