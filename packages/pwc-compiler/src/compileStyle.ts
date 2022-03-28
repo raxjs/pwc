ad78c70ebedb7d01494cec1e3548fba44a74fd17
逆葵 <xianyong.yxy@alibaba-inc.com>
2022-03-28 19:52:28 +0800

    feat(compiler): add postcss to compile css

--- packages/pwc-compiler/src/compileStyle.ts
+++ packages/pwc-compiler/src/compileStyle.ts
@@ -1,6 +1,46 @@
+import postcss, { SourceMap, ProcessOptions } from 'postcss';
+import { RawSourceMap } from 'source-map';
 import type { SFCDescriptor } from './parse';
 
-export function compileStyle(descriptor: SFCDescriptor): string {
-  // TODO: add postcss
-  return descriptor.style.content;
+// type definition in postcss (source-map-js) is different from source-map v0.7
+interface AdaptorRawSourceMap extends Omit<RawSourceMap, 'version' | 'file'> {
+  version: string;
+}
+export interface SFCStyleCompileResults {
+  code: string;
+  map: AdaptorRawSourceMap | null;
+  errors: Error[];
+}
+
+export function compileStyle(descriptor: SFCDescriptor): SFCStyleCompileResults {
+  const source = descriptor.style.content;
+  const filename = descriptor.filename;
+  const errors: Error[] = [];
+  const plugins = []; // TODO:
+  let result;
+  let code: string | undefined;
+  let outMap: SourceMap | undefined;
+  const postCSSOptions: ProcessOptions = {
+    to: filename,
+    from: filename,
+    map: {
+      inline: false,
+      annotation: false
+    }
+  };
+
+  try {
+    result = postcss(plugins).process(source, postCSSOptions);
+  } catch (err) {
+    errors.push(err);
+  }
+
+  code = result.css;
+  outMap = result.map;
+
+  return {
+    code: code || '',
+    map: outMap && outMap.toJSON(),
+    errors
+  }
 }
--- packages/rollup-plugin-pwc/src/style.ts
+++ packages/rollup-plugin-pwc/src/style.ts
@@ -2,16 +2,22 @@ import { compileStyle } from '@pwc/compiler';
 import type { TransformPluginContext } from 'rollup';
 import { getDescriptor } from './utils/descriptorCache';
 import type { StyleBlockQuery } from './utils/query';
+import { createRollupError } from './utils/error';
 
 export function transformStyle(
   query: StyleBlockQuery,
   pluginContext: TransformPluginContext,
 ) {
   const descriptor = getDescriptor(query.filename);
-  // TODO: result should include errors if using postcss
-  const result = compileStyle(descriptor);
+  const { code, map, errors } = compileStyle(descriptor);
+  if (errors.length) {
+    errors.forEach(error => {
+      pluginContext.error(createRollupError(descriptor.filename, error));
+    });
+    return null;
+  }
   return {
-    code: result,
-    map: null,
+    code,
+    map
   };
 }
