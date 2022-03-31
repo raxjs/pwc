cc50b0d746a708193fdc2d6e1239c4733374b74a
逆葵 <xianyong.yxy@alibaba-inc.com>
2022-03-31 19:04:02 +0800

    feat(compiler): finish compileTemplateInRuntime method

--- packages/pwc-compiler/src/compileTemplateInRuntime.ts
+++ packages/pwc-compiler/src/compileTemplateInRuntime.ts
@@ -7,13 +7,29 @@ interface compileTemplateInRuntimeResult {
   [1]: compileTemplateResult['values'];
 }
 
-export function compileTemplateInRuntime(template: string): compileTemplateInRuntimeResult {
+function injectRuntimeValue(bindings, originalValues): compileTemplateResult['values'] {
+  let bindingIndex = 0;
+  return originalValues.map(val => {
+    if (typeof val === 'string') {
+      return bindings[bindingIndex++];
+    } else {
+      val.forEach(obj => {
+        obj.value = bindings[bindingIndex++];
+      });
+      return val;
+    }
+  }) as compileTemplateResult['values'];
+}
+
+export function compileTemplateInRuntime(strings: Array<string>, ...runtimeValues): compileTemplateInRuntimeResult {
+  const template = strings.join('{{}}');
   let dom;
   try {
     dom = parseFragment(template);
   } catch (err) {
     throw new Error(`template compile error: ${err}`);
   }
-  const { templateString, values } = compileTemplateAST(dom);
-  return [templateString, values];
+  const { templateString, values: originalValues } = compileTemplateAST(dom);
+  const values = injectRuntimeValue(runtimeValues, originalValues);
+  return [ templateString, values ];
 }
