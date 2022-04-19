6adb50935497eb193c6e6cd7320b4d5ee7102d4e
chenrongyan.cry <chenrongyan.cry@alibaba-inc.com>
2022-04-19 12:17:07 +0800

    fix: export toRaw

--- packages/pwc/src/index.ts
+++ packages/pwc/src/index.ts
@@ -2,6 +2,6 @@ import './elements';
 
 export { nextTick } from './elements/sheduler';
 export * from './decorators';
-import { toRaw } from './utils';
+export { toRaw } from './utils';
 
 export { compileTemplateInRuntime as html } from '@pwc/compiler/compileTemplateInRuntime';
