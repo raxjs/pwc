d8ddffdaf7fade339af71249547624cddfe81a09
chenrongyan.cry <chenrongyan.cry@alibaba-inc.com>
2022-04-19 11:24:17 +0800

    feat: add reactive docs and export toRaw

--- packages/pwc/src/index.ts
+++ packages/pwc/src/index.ts
@@ -2,5 +2,6 @@ import './elements';
 
 export { nextTick } from './elements/sheduler';
 export * from './decorators';
+export { toRaw } from './utils/toRaw';
 
 export { compileTemplateInRuntime as html } from '@pwc/compiler/compileTemplateInRuntime';
