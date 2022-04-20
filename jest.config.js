33ec3192011756a1031907fe3e8688f9b0bc99a0
chenrongyan.cry <chenrongyan.cry@alibaba-inc.com>
2022-04-20 17:45:17 +0800

    chore: jest coverage

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   coverageDirectory: './coverage/',
   collectCoverage: true,
-  collectCoverageFrom: ['packages/*/lib/*.{js,jsx}'],
+  collectCoverageFrom: ['packages/*/src/**/*.{js,ts}'],
   coveragePathIgnorePatterns: ['<rootDir>/node_modules/'],
   roots: ['<rootDir>/packages'],
-  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/es/', '/dist/', '.d.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/cjs/', '/esm/', '/es2017/', '/dist/', '.d.ts'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   testEnvironment: 'jsdom',
   transform: {
