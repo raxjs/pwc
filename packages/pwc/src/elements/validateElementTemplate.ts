5dae0569caa6ab2aacb41b21f3ea21440feaf1c2
fushen <fushen.jzw@alibaba-inc.com>
2022-04-26 15:22:10 +0800

    feat: html method support new structure

--- packages/pwc/src/elements/validateElementTemplate.ts
+++ packages/pwc/src/elements/validateElementTemplate.ts
@@ -3,7 +3,7 @@ import { ElementTemplate } from '../type';
 
 export function validateElementTemplate(elementTemplate: ElementTemplate) {
   if (typeof elementTemplate === 'string') return;
-  console.log(elementTemplate)
+  console.log(elementTemplate);
 
   if (elementTemplate.template !== true) {
     throwError('Invalid template, it should return type field.');
