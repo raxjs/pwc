7d2516c7041063e0af2f69d3c09936d13bfb4d70
chenrongyan.cry <chenrongyan.cry@alibaba-inc.com>
2022-05-16 11:35:44 +0800

    fix: add global jest

--- packages/pwc/src/reactivity/__tests__/createReactive/WeakMap.test.ts
+++ packages/pwc/src/reactivity/__tests__/createReactive/WeakMap.test.ts
@@ -1,3 +1,4 @@
+import { jest } from '@jest/globals';
 import { createReactive } from '../../createReactive';
 
 describe('createReactive/WeakMap', () => {
--- packages/pwc/src/reactivity/__tests__/createReactive/base.test.ts
+++ packages/pwc/src/reactivity/__tests__/createReactive/base.test.ts
@@ -1,3 +1,4 @@
+import { jest } from '@jest/globals';
 import { getProperties, toRaw } from '../../../utils';
 import { createReactive } from '../../createReactive';
 
